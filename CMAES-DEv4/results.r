c4cd1626eb4413cf898e82b08e0bba0ee33a9cba
Jagorius <jagorius@gmail.com>
2015-03-12 22:39:36 +0100

    wersja 4, 4,5 i 4,6

--- CMAES-DEv4/results.r
+++ CMAES-DEv4/results.r
@@ -1,5 +1,6 @@
 results <- function(){
   
+  cmade46<- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult46.txt")  
   cmade45<- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult45.txt")  
   cmade4 <- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult.txt")  
   cmade3 <- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv3/Cresult.txt")
@@ -14,10 +15,10 @@ results <- function(){
   cmade45vs3[1001]  <- sum(cmade45vs3)
   cmade45vs3[1002]  <- median(cmade45vs3)
   
-  cmade45vs4        <- array(0,1000)
-  cmade45vs4        <- cmade4[,4] - cmade45[,4]
-  cmade45vs4[1001]  <- sum(cmade45vs4)
-  cmade45vs4[1002]  <- median(cmade45vs4)
+  cmade46vs3        <- array(0,1000)
+  cmade46vs3        <- cmade3[,4] - cmade46[,4]
+  cmade46vs3[1001]  <- sum(cmade46vs3)
+  cmade46vs3[1002]  <- median(cmade46vs3)
   
   resmatrix <- matrix(0, nrow=1002, ncol=3)
   #resmatrix[,1] <- cmade3[,4]
@@ -25,12 +26,21 @@ results <- function(){
   #resmatrix[,3] <- cmade4vs3
   resmatrix[,1] <- cmade4vs3
   resmatrix[,2] <- cmade45vs3
-  resmatrix[,3] <- cmade45vs4
+  resmatrix[,3] <- cmade46vs3
   
-  colnames(resmatrix) <- c("CMADEv3-CMADEv4  ","CMADEv3-CMADEv4,5   ","CMADEv4-CMADEv4,5")
+  colnames(resmatrix) <- c("CMADEv3-CMADEv4  ","CMADEv3-CMADEv4,5   ","CMADEv3-CMADEv4,6")
   rownames(resmatrix) <- c(0:999,"SUM", "MEDIAN")
   resmatrix <- as.table(resmatrix)
   
-  capture.output(print(resmatrix, print.gap=8), file="C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cv3vsCv4vsCv45.txt")
+  sink("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cv3vsCv4vsCv45vsCv46.txt")
+  print(resmatrix, print.gap=8)
+  cat("\n\nLEGENDA:\n\tCMADE4:\t Wagi niejednakowe, par<0.8lower,0.8upper>\t
+      \tCMADE4,5:Wagi jednakowe, par<lower,upper>\t
+      \tCMADE4,6:Wagi jednakowe, par<0.8lower,0.8upper>\t")
+  sink()
+  #capture.output(print(resmatrix, print.gap=8), file="C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cv3vsCv4vsCv45.txt")
   
+  #fileConn<-file("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cv3vsCv4vsCv45.txt")
+  #writeLines(c("Hello","World"), fileConn)
+  #close(fileConn)
 }
