663966e794439ddbb66084f8924f42a4e8cbde9d
Jagorius <jagorius@gmail.com>
2015-03-12 11:59:01 +0100

    wersja 4 i 4,5

--- CMAES-DEv4/results.r
+++ CMAES-DEv4/results.r
@@ -1,25 +1,36 @@
 results <- function(){
   
+  cmade45<- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult45.txt")  
   cmade4 <- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult.txt")  
   cmade3 <- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv3/Cresult.txt")
   
-  cmade4vs3 <- array(0,1000)
-  cmade4vs3 <- cmade3[,4] - cmade4[,4]
+  cmade4vs3         <- array(0,1000)
+  cmade4vs3         <- cmade3[,4] - cmade4[,4]
+  cmade4vs3[1001]   <- sum(cmade4vs3)
+  cmade4vs3[1002]   <- median(cmade4vs3)
   
-  resmatrix <- matrix(0, nrow=1000, ncol=3)
-  resmatrix[,1] <- cmade3[,4]
-  resmatrix[,2] <- cmade4[,4]
-  resmatrix[,3] <- cmade4vs3
+  cmade45vs3        <- array(0,1000)
+  cmade45vs3        <- cmade3[,4] - cmade45[,4]
+  cmade45vs3[1001]  <- sum(cmade45vs3)
+  cmade45vs3[1002]  <- median(cmade45vs3)
   
-  colnames(resmatrix) <- c("CMADEv3-MIN","CMADEv4-MIN","CMADEv3-CMADEv4")
-  rownames(resmatrix) <- c(0:999)
+  cmade45vs4        <- array(0,1000)
+  cmade45vs4        <- cmade4[,4] - cmade45[,4]
+  cmade45vs4[1001]  <- sum(cmade45vs4)
+  cmade45vs4[1002]  <- median(cmade45vs4)
+  
+  resmatrix <- matrix(0, nrow=1002, ncol=3)
+  #resmatrix[,1] <- cmade3[,4]
+  #resmatrix[,2] <- cmade4[,4]
+  #resmatrix[,3] <- cmade4vs3
+  resmatrix[,1] <- cmade4vs3
+  resmatrix[,2] <- cmade45vs3
+  resmatrix[,3] <- cmade45vs4
+  
+  colnames(resmatrix) <- c("CMADEv3-CMADEv4  ","CMADEv3-CMADEv4,5   ","CMADEv4-CMADEv4,5")
+  rownames(resmatrix) <- c(0:999,"SUM", "MEDIAN")
   resmatrix <- as.table(resmatrix)
   
-  write.table(resmatrix, "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cv3vsCv4.txt", sep="\t\t")
-  
-  print(sum(cmade4vs3))
-  print(mean(cmade4vs3))
-  print(median(cmade4vs3))
-  
+  capture.output(print(resmatrix, print.gap=8), file="C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cv3vsCv4vsCv45.txt")
   
 }
