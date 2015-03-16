36d3193953cdd8d075625c4ff6192f777b1c7bfd
Jagorius <jagorius@gmail.com>
2015-03-16 19:03:06 +0100

    Najnowsze wyniki 4,9

--- CMAES-DEv4/results.r
+++ CMAES-DEv4/results.r
@@ -1,5 +1,6 @@
 results <- function(){
   
+  cmade49<- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult49.txt")  
   cmade48<- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult48.txt")  
   cmade47<- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult47.txt")  
   cmade46<- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult46.txt")  
@@ -32,7 +33,12 @@ results <- function(){
   cmade48vs3[1001]  <- sum(cmade48vs3)
   cmade48vs3[1002]  <- median(cmade48vs3)
   
-  resmatrix <- matrix(0, nrow=1002, ncol=5)
+  cmade49vs3        <- array(0,1000)
+  cmade49vs3        <- cmade3[,4] - cmade49[,4]
+  cmade49vs3[1001]  <- sum(cmade49vs3)
+  cmade49vs3[1002]  <- median(cmade49vs3)
+  
+  resmatrix <- matrix(0, nrow=1002, ncol=6)
   #resmatrix[,1] <- cmade3[,4]
   #resmatrix[,2] <- cmade4[,4]
   #resmatrix[,3] <- cmade4vs3
@@ -41,8 +47,10 @@ results <- function(){
   resmatrix[,3] <- cmade46vs3
   resmatrix[,4] <- cmade47vs3
   resmatrix[,5] <- cmade48vs3
+  resmatrix[,6] <- cmade49vs3
   
-  colnames(resmatrix) <- c("CMADEv3-CMADEv4  ","CMADEv3-CMADEv4,5   ","CMADEv3-CMADEv4,6","CMADEv3-CMADEv4,7","CMADEv3-CMADEv4,8")
+  
+  colnames(resmatrix) <- c("CMADEv3-CMADEv4  ","CMADEv3-CMADEv4,5   ","CMADEv3-CMADEv4,6","CMADEv3-CMADEv4,7","CMADEv3-CMADEv4,8","CMADEv3-CMADEv4,9")
   rownames(resmatrix) <- c(0:999,"SUM", "MEDIAN")
   resmatrix <- as.table(resmatrix)
   
@@ -52,7 +60,8 @@ results <- function(){
       \tCMADE4,5:Wagi jednakowe, par<lower,upper>,odwrotne pathRatio\t
       \tCMADE4,6:Wagi jednakowe, par<0.8lower,0.8upper>,odwrotne pathRatio\t
       \tCMADE4,7:Wagi niejednakowe, poprawne pathRatio\t
-      \tCMADE4,8:Wagi niejednakowe,par<0.8lower,0.8upper>,nowa wartoœæ FtInit, poprawne pathRatio")
+      \tCMADE4,8:Wagi niejednakowe,par<0.8lower,0.8upper>,nowa wartoœæ FtInit, poprawne pathRatio
+      \tCMADE4,9:Wagi niejednakowe,populaton<-replicate(rnorm),FtInit=5, poprawne pathRatio")
   sink()
   #capture.output(print(resmatrix, print.gap=8), file="C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cv3vsCv4vsCv45.txt")
   
