fd164bad48820f0849e31c4767816646c6e87591
Jagorius <jagorius@gmail.com>
2015-03-19 10:39:40 +0100

    Najnowsze wyniki

--- CMAES-DEv5/results.r
+++ CMAES-DEv5/results.r
@@ -1,5 +1,6 @@
 results <- function(){
   
+  cmade52 <- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv5/Cresult52.txt")  
   cmade51 <- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv5/Cresult51.txt")  
   cmade5  <- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv5/Cresult5.txt")  
   cmade4  <- read.table("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cresult49.txt")  
@@ -14,22 +15,29 @@ results <- function(){
   cmade51vs4[1001]   <- sum(cmade51vs4)
   cmade51vs4[1002]   <- median(cmade51vs4)
   
-  resmatrix <- matrix(0, nrow=1002, ncol=2)
+  cmade52vs4         <- array(0,1000)
+  cmade52vs4         <- cmade4[,4] - cmade52[,4]
+  cmade52vs4[1001]   <- sum(cmade52vs4)
+  cmade52vs4[1002]   <- median(cmade52vs4)
+  
+  resmatrix <- matrix(0, nrow=1002, ncol=3)
   #resmatrix[,1] <- cmade3[,4]
   #resmatrix[,2] <- cmade4[,4]
   #resmatrix[,3] <- cmade4vs3
   resmatrix[,1] <- cmade5vs4
   resmatrix[,2] <- cmade51vs4
+  resmatrix[,3] <- cmade52vs4
   
   
-  colnames(resmatrix) <- c("CMADEv4-CMADEv5", "CMADEv4-CMADEv51")
+  colnames(resmatrix) <- c("CMADEv4-CMADEv5", "CMADEv4-CMADEv51", "CMADEv4-CMADEv52")
   rownames(resmatrix) <- c(0:999,"SUM", "MEDIAN")
   resmatrix <- as.table(resmatrix)
   
   sink("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv5/Cv4vsCv5.txt")
   print(resmatrix, print.gap=8)
   cat("\n\nLEGENDA:\n\tCMADE5 :\t c_Ft=1/sqrt(N)
-      \tCMADE51:\t c_Ft=2/sqrt(N)")
+      \tCMADE51:\t c_Ft=2/sqrt(N)
+      \tCMADE51:\t pathRatio<- stale, nowe c_Ft (wektor chi)")
   sink()
   #capture.output(print(resmatrix, print.gap=8), file="C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEv4/Cv3vsCv4vsCv45.txt")
   
