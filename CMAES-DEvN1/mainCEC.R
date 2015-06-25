26f73b6036f082dd13c4c8bcc39ab3f64a4e0342
Jagorius <jagorius@gmail.com>
2015-06-25 21:35:13 +0200

    skrypt do wyznaczania srednich bledow w zaleznosci od wielkosci populacji na CEC

--- CMAES-DEvN1/mainCEC.R
+++ CMAES-DEvN1/mainCEC.R
@@ -1,22 +1,45 @@
-mainCEC <- function(dim) {
+mainCEC <- function(dim,lambda) {
   
   library("cec2013")
   
-  source("CMADE-vN1.R")
-  cat("Dimensions =", dim,"\n") 
-  cat("                 TIME-MIN,       BEST,        RESTARTS","\n")
-  for (i in 1:28){ 
+  source("C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEvN1/CMADE-vN1.R")
+  cat("                 TIME-MIN,       BEST,      ERR,       RESTARTS","\n")
+  RES = rep (-1,28)
+  for (i in 1:1){ 
     
     start.time  <- Sys.time()
-    startPoint <- rep(0,dim)
-    result <- CMADEN1(startPoint,fn=function(x){cec2013(i,x)},control=list("budget"=length(startPoint)*100))  
+    result <- CMADEN1(rep(0,dim),fn=function(x){cec2013(i,x)},control=list("budget"=dim*100, "lambda" = lambda))  
     time.taken  <- Sys.time() - start.time
-    cat("Function ",i,":\t",as.numeric(time.taken, units = "mins")," , ",result$value," , ",result$countreset, "\n")
+    err = abs(result$value - (-1400 + 100*(i-1)) ) 
+    if(i>15) err = err + 100
+    RES[i] <- err
+    cat("DIM=",dim," Function ",i,":\t",as.numeric(time.taken, units = "mins")," , ", result$value, " , " ,err," , ",result$countreset, "\n")
     
   }
-  cat("\n\n")
-  
+  # Return average error 
+  return(mean(RES))
 }
-mainCEC(10)
-mainCEC(30)
-mainCEC(50)
+
+mat10 <- matrix(-1, 100, 2)
+N <- 10
+for(l in 1:100){
+  err <- mainCEC(N,l+N-1)
+  mat10[l,] <- c(l+N-1,err)
+}
+write.csv(mat10, file = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEvN1/res10.csv")
+
+mat30 <- matrix(-1, 100, 2)
+N <- 30
+for(l in 1:100){
+  err <- mainCEC(N,l+N-1)
+  mat10[l,] <- c(l+N-1,err)
+}
+write.csv(mat30, file = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEvN1/res30.csv")
+
+mat50 <- matrix(-1, 100, 2)
+N <- 50
+for(l in 1:100){
+  err <- mainCEC(N,l+N-1)
+  mat10[l,] <- c(l+N-1,err)
+}
+write.csv(mat50, file = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CMAES-DEvN1/res50.csv")
