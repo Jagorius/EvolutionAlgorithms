8ac88a1be201a42300466b02be5f7c8410b88a50
Jagorius <Jagorius@gmail.com>
2016-12-28 11:26:07 +0100

    CEC2017

---
+++ CEC2017/CEC2017ParallelBenchmark.R
@@ -0,0 +1,83 @@
+benchmarkParallelCMADE <- function() {
+  library(foreach)
+  library(doParallel)
+  
+  start.time  <- Sys.time()
+  
+  # Minimal fitness values for each problem
+  scores <- seq(from = 100, to = 3000, by=100)
+  
+  # Calculate the number of cores
+  no_cores <- detectCores() - 1
+  
+  # Initiate cluster
+  registerDoParallel(no_cores)
+  
+  # For each of problem dimmension
+  for(d in c(10,30)){
+    # Make parallel computing for each of 30 problems
+    results = foreach(n = 1:30, 
+                      .combine = c,
+                      .export = c("scores","d") )  %dopar%  {
+                        
+                        source('CMADEv12.R')
+                        library(cec2017)
+                        resultVector <- c()
+                        # 51 runs per problem
+                        for(i in 1:51){
+			  result <- tryCatch(
+				{
+					CMADE(
+						rep(0,d),
+						fn=function(x){
+							cec2017(n,x)
+						},
+						lower=-100,
+						upper=100
+					)
+				},
+				error=function(cond) {
+					print(paste("Problem:", d," ",cond))
+				}
+
+			  )   
+                          
+                          resultVector <- c(resultVector, abs(result$value-scores[n]))
+                        }
+                        write.table(resultVector, file = paste("N/N",n,"-D",d,sep=""), sep = ",")
+                        
+                        return( paste("CEC2017 N=",n," D=",d, " --> Best: ", min(resultVector)," Worst: ",max(resultVector)," Mean: ", mean(resultVector)," Sd: ",sd(resultVector),sep="") )
+                      }
+    print(results)
+    
+  }
+  stopImplicitCluster()
+  time.taken  <- Sys.time() - start.time
+  print(paste("Calculation time[hours]: ",as.numeric(time.taken, units = "hours")))
+}
+
+CEC2017tableCreate <- function(x){
+  
+  path <- getwd()
+  
+  for(n in c(10,30,50,100)){
+    csvResults <-  matrix(0, nrow = 51, ncol = 1)
+    if(file.exists(paste(path,"/N/N1-D",n,sep="")) ){
+      for(i in 1:30){
+        resColumn <- read.table(paste(path,"/N/N",i,"-D",n,sep=""), sep=",",header = TRUE)
+        colnames(resColumn) <- paste("P",i,sep = "")
+        csvResults <- cbind(csvResults,resColumn)
+      }
+      csvResults <- csvResults[,-1]
+      print(csvResults)
+      
+      write.csv(csvResults, file = paste(path,paste("/resTable-",n,".csv",sep=""),sep=""), row.names = TRUE)
+    }
+  }
+  
+  
+}
+
+## Rscript convention
+benchmarkParallelCMADE()
+CEC2017tableCreate()
