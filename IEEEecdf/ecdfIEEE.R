f4eb14b57d155138c0e87d5a4ddaf073be6fbec4
Jagorius <Jagorius@gmail.com>
2017-12-28 20:27:52 +0100

    ecdf plots 5x5

--- IEEEecdf/ecdfIEEE.R
+++ IEEEecdf/ecdfIEEE.R
@@ -7,98 +7,119 @@ ecdfIEEE<- function(n,f_from,f_to){
   
   print(paste("Dimension: ",n,sep=''))
   
-  des_path = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CEC2017/CMADE-BBComp5 HistSize=3sqrt(N) init(-80,80) FINALTEST/M/"
+  #des_path = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CEC2017/CMADE-BBComp5 HistSize=3sqrt(N) init(-80,80) FINALTEST/M/"
   rb_ipop_cma_es_path = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/IEEEecdf/Results for all papers/paper ID  E-17343/RB-IPOP-CMA-ES/"
   cmaes_pure = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CEC2017/CMAES/M/"
   des_pathv2 = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/DESv2/DES + dMean custom1 (DESv2.5)/M/"
   jsopath = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/IEEEecdf/Results for all papers/Paper ID   17315/jSO/"
   lshadeSpacma_path = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/IEEEecdf/Results for all papers/Paper ID 17051/LSHADE_SPACMA/"
-  cmaesNos_path = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CEC2017/CMAES noSigma/M/"
+  #cmaesNos_path = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/CEC2017/CMAES noSigma/M/"
   ebopath = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/IEEEecdf/Results for all papers/EBOwithCMAR/data for email/"
   idepath = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/IEEEecdf/Results for all papers/Paper ID  E-17322/IDEbestNsize/"
   ppsopath = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/IEEEecdf/Results for all papers/paper ID E-17447/cec2017PPSO/"
-  
+  mmoedpath = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/IEEEecdf/Results for all papers/Paper ID  17321/Multi-method based Orthogonal Experimental Design Algorithm for Solving CEC2017 Competition Problems results/"
+  dyypopath = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/IEEEecdf/Results for all papers/Paper ID 17543/DYYPO/"
+  tlboflpath = "C:/Users/JS/Desktop/Doktorat/EvolutionAlgorithms/IEEEecdf/Results for all papers/Paper ID 17544/TLBO-FL/"
+
   ecdfValues <- list()
   budgetSteps <- c(0.01, 0.02, 0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)*log10(10000)
-  colors <- rainbow(11) 
-  colors[3] <- colors[4]
-  colors[4] <- colors[11]
-  linetype <- c(1:10) 
+  colors <- rainbow(16) 
+  colors[3] <- colors[16]
+  linetype <- c(c(1:10),c(6:1))
   plotchar <- seq(15,15+10,1)
 
   ecdfMaxSucess <- 0
-  resultsDES <- list()
+  #resultsDES <- list()
   resultsRB_IPOP_CMA_ES <- list()
   resultsCmaes_pure <- list()
   resultsDESv2 <- list()
   resultsjso <- list()
   resultslshadeSpacma <- list()
-  resultsCmaesNos <- list()
+  #resultsCmaesNos <- list()
   resultsEbo <- list()
   resultsIDE <- list()
   resultsPPSO <- list()
+  resultsMMOED <- list()
+  resultsDYYPO <- list()
+  resultsTLBOFL <- list()
   
   for(p in 1:30){
-    resultsDES[[p]] <- read.table(file = paste(des_path,"DES_",p,"_",N,".txt",sep=""),sep = ",")
+    #resultsDES[[p]] <- read.table(file = paste(des_path,"DES_",p,"_",N,".txt",sep=""),sep = ",")
     resultsRB_IPOP_CMA_ES[[p]] <- read.table(file = paste(rb_ipop_cma_es_path,"RB-IPOP-CMA-ES_",p,"_",N,".txt",sep=""),sep = " ")
     resultsCmaes_pure[[p]] <- read.table(file = paste(cmaes_pure,"CMAES_",p,"_",N,".txt",sep=""),sep = ",",header = TRUE)
     resultsDESv2[[p]] <- read.table(file = paste(des_pathv2,"DES_",p,"_",N,".txt",sep=""),sep = ",",header = TRUE)
     resultsjso[[p]] <- read.table(file = paste(jsopath,"jSO_",p,"_",N,".txt",sep=""))
     resultslshadeSpacma[[p]] <- read.table(file = paste(lshadeSpacma_path,"LSHADE_SPACMA_",p,"_",N,".txt",sep=""))
-    resultsCmaesNos[[p]] <- read.table(file = paste(cmaesNos_path,"CMAES_",p,"_",N,".txt",sep=""),sep = ",",header = TRUE)
+    #resultsCmaesNos[[p]] <- read.table(file = paste(cmaesNos_path,"CMAES_",p,"_",N,".txt",sep=""),sep = ",",header = TRUE)
     resultsEbo[[p]] <- read.table(file = paste(ebopath,"EBOwithCMAR_",p,"_",N,".dat",sep=""))
     resultsIDE[[p]] <- read.table(file = paste(idepath,"IDEbestNsize_",N,"_",p,".txt",sep=""))
     resultsPPSO[[p]] <- read.table(file = paste(ppsopath,"PPSO_",p,"_",N,".txt",sep=""))
+    resultsMMOED[[p]] <- read.table(file = paste(mmoedpath,"MM_OED_",p,"_",N,".txt",sep=""))
+    resultsDYYPO[[p]] <- read.table(file = paste(dyypopath,"DYYPO_",p,"_",N,".txt",sep=""))
+    resultsTLBOFL[[p]] <- read.table(file = paste(tlboflpath,"TLBO-FL_",p,"_",N,".txt",sep=""))
     
     ecdfValues[[p]] <- rev(c(1 %o% (10)^(0.2*((log10(max(min(
-                                                        min(resultsDES[[p]][14,]),
+                                                        #min(resultsDES[[p]][14,]),
                                                         min(resultsRB_IPOP_CMA_ES[[p]][14,]),
                                                         min(resultsCmaes_pure[[p]][14,]),
                                                         min(resultsDESv2[[p]][14,]),
                                                         min(resultsjso[[p]][14,]),
                                                         min(resultslshadeSpacma[[p]][14,]),
-                                                        min(resultsCmaesNos[[p]][14,]),
+                                                        #min(resultsCmaesNos[[p]][14,]),
                                                         min(resultsEbo[[p]][14,]),
                                                         min(resultsIDE[[p]][14,]),
-                                                        min(resultsPPSO[[p]][14,])
+                                                        min(resultsPPSO[[p]][14,]),
+                                                        min(resultsMMOED[[p]][14,]),
+                                                        min(resultsDYYPO[[p]][14,]),
+                                                        min(resultsTLBOFL[[p]][14,])
                                                             ),10^-8)  )/0.2):(log10(max(
-                                                                 max(resultsDES[[p]][1,]),
+                                                                 #max(resultsDES[[p]][1,]),
                                                                  max(resultsRB_IPOP_CMA_ES[[p]][1,]),
                                                                  max(resultsCmaes_pure[[p]][1,]),
                                                                  max(resultsDESv2[[p]][1,]),
                                                                  max(resultsjso[[p]][1,]),
                                                                  max(resultslshadeSpacma[[p]][1,]),
-                                                                 max(resultsCmaesNos[[p]][1,]),
+                                                                 #max(resultsCmaesNos[[p]][1,]),
                                                                  max(resultsEbo[[p]][1,]),
                                                                  max(resultsIDE[[p]][1,]),
-                                                                 max(resultsPPSO[[p]][1,])
+                                                                 max(resultsPPSO[[p]][1,]),
+                                                                 max(resultsMMOED[[p]][1,]),
+                                                                 max(resultsDYYPO[[p]][1,]),
+                                                                 max(resultsTLBOFL[[p]][1,])
                                                                  )  )/0.2) ))))
       }
   
-  minCountDES <- rep(0,length(budgetSteps))
+  #minCountDES <- rep(0,length(budgetSteps))
   minCountRB_IPOP_CMA_ES <- rep(0,length(budgetSteps))
   minCountCmaes_pure <- rep(0,length(budgetSteps))
   minCountDESv2 <- rep(0,length(budgetSteps))
   minCountjso <- rep(0,length(budgetSteps))
   minCountshadeSpacma <- rep(0,length(budgetSteps))
-  minCountCmaes_nos <- rep(0,length(budgetSteps))
+  #minCountCmaes_nos <- rep(0,length(budgetSteps))
   minCountEbo <- rep(0,length(budgetSteps))
   minCountIde <- rep(0,length(budgetSteps))
   minCountPpso <- rep(0,length(budgetSteps))
+  minCountMmoed <- rep(0,length(budgetSteps))
+  minCountDyypo <- rep(0,length(budgetSteps))
+  minCountTlbofl <- rep(0,length(budgetSteps))
+  
   for(p in F_from:F_to){
     print(paste("Calculating for function: ",p))
     for(b in 1:length(budgetSteps)){
       for(e in 1:length(ecdfValues[[p]])){
-        minCountDES[b] <- minCountDES[b] + sum(resultsDES[[p]][b,]<ecdfValues[[p]][e])
+        #minCountDES[b] <- minCountDES[b] + sum(resultsDES[[p]][b,]<ecdfValues[[p]][e])
         minCountRB_IPOP_CMA_ES[b] <- minCountRB_IPOP_CMA_ES[b] + sum(resultsRB_IPOP_CMA_ES[[p]][b,]<ecdfValues[[p]][e])
         minCountCmaes_pure[b] <- minCountCmaes_pure[b] + sum(resultsCmaes_pure[[p]][b,]<ecdfValues[[p]][e])
         minCountDESv2[b] <- minCountDESv2[b] + sum(resultsDESv2[[p]][b,]<ecdfValues[[p]][e])
         minCountjso[b] <- minCountjso[b] + sum(resultsjso[[p]][b,]<ecdfValues[[p]][e])
         minCountshadeSpacma[b] <- minCountshadeSpacma[b] + sum(resultslshadeSpacma[[p]][b,]<ecdfValues[[p]][e])
-        minCountCmaes_nos[b] <- minCountCmaes_nos[b] + sum(resultsCmaesNos[[p]][b,]<ecdfValues[[p]][e])
+        #minCountCmaes_nos[b] <- minCountCmaes_nos[b] + sum(resultsCmaesNos[[p]][b,]<ecdfValues[[p]][e])
         minCountEbo[b] <- minCountEbo[b] + sum(resultsEbo[[p]][b,]<ecdfValues[[p]][e])
         minCountIde[b] <- minCountIde[b] + sum(resultsIDE[[p]][b,]<ecdfValues[[p]][e])
         minCountPpso[b] <- minCountPpso[b] + sum(resultsPPSO[[p]][b,]<ecdfValues[[p]][e])
+        minCountMmoed[b] <- minCountMmoed[b] + sum(resultsMMOED[[p]][b,]<ecdfValues[[p]][e])
+        minCountDyypo[b] <- minCountDyypo[b] + sum(resultsDYYPO[[p]][b,]<ecdfValues[[p]][e])
+        minCountTlbofl[b] <- minCountTlbofl[b] + sum(resultsTLBOFL[[p]][b,]<ecdfValues[[p]][e])
         
       }
     }
@@ -106,20 +127,21 @@ ecdfIEEE<- function(n,f_from,f_to){
   }
   
   setEPS()
-  postscript( paste("Problems",F_from,"-",F_to,",N=",N,".eps",sep=""), width = 8, height = 8)
+  postscript( paste("Problems",F_from,"-",F_to,",N=",N,".eps",sep=""), width = 5, height = 5)
+                                              #xlab="log10 of (f-evals / dimension)",ylab="Proportion of function + target pairs",
+  plot(budgetSteps,minCountDESv2/(ecdfMaxSucess),xlab="",ylab="",ylim=c(0, 1),type="b", lwd=2,lty=linetype[1], col=colors[1], pch=plotchar[1])
+  lines(budgetSteps,minCountCmaes_pure/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[2], col=colors[2], pch=plotchar[2])
+  lines(budgetSteps,minCountRB_IPOP_CMA_ES/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[3], col=colors[3], pch=plotchar[3])
+  lines(budgetSteps,minCountjso/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[4], col=colors[4], pch=plotchar[4])
+  lines(budgetSteps,minCountshadeSpacma/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[5], col=colors[5], pch=plotchar[5])
+  lines(budgetSteps,minCountEbo/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[6], col=colors[6], pch=plotchar[6])
+  lines(budgetSteps,minCountIde/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[7], col=colors[7], pch=plotchar[7])
+  lines(budgetSteps,minCountPpso/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[8], col=colors[8], pch=plotchar[8])
+  lines(budgetSteps,minCountMmoed/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[9], col=colors[9], pch=plotchar[9])
+  lines(budgetSteps,minCountDyypo/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[10], col=colors[10], pch=plotchar[10])
+  lines(budgetSteps,minCountTlbofl/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[11], col=colors[11], pch=plotchar[11])
   
-  plot(budgetSteps,minCountDES/(ecdfMaxSucess),xlab="log10 of (f-evals / dimension)",ylab="Proportion of function + target pairs",ylim=c(0, 1),type="b", lwd=2,lty=linetype[1], col=colors[1], pch=plotchar[1])
-  lines(budgetSteps,minCountRB_IPOP_CMA_ES/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[2], col=colors[2], pch=plotchar[2])
-  lines(budgetSteps,minCountCmaes_pure/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[3], col=colors[3], pch=plotchar[3])
-  lines(budgetSteps,minCountDESv2/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[4], col=colors[4], pch=plotchar[4])
-  lines(budgetSteps,minCountjso/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[5], col=colors[5], pch=plotchar[5])
-  lines(budgetSteps,minCountshadeSpacma/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[6], col=colors[6], pch=plotchar[6])
-  lines(budgetSteps,minCountCmaes_nos/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[7], col=colors[7], pch=plotchar[7])
-  lines(budgetSteps,minCountEbo/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[8], col=colors[8], pch=plotchar[8])
-  lines(budgetSteps,minCountIde/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[9], col=colors[9], pch=plotchar[9])
-  lines(budgetSteps,minCountPpso/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[10], col=colors[10], pch=plotchar[10])
-  
-  legend(-0.08, 1.03, c('DESv1', 'RB_IPOP_CMA_ES', 'CMAES','DESv2','jSO','LSHADE_SPACMA','CMAES w/o Sigma','EBOwithCMAR','IDEbestNsize','PPSO'), cex=0.8, col=colors[1:10],pch=plotchar[1:10], lty=linetype[1:10] )
+  legend(-0.08, 1.03, c('DESv2', 'CMAES','RB_IPOP_CMA_ES','jSO','LSHADE_SPACMA','EBOwithCMAR','IDEbestNsize','PPSO','MM_OED','DYYPO','TLBO-FL'), text.font=2, cex=0.8, col=colors[1:11],pch=plotchar[1:11], lty=linetype[1:11] )
   
   dev.off()
   
@@ -127,6 +149,7 @@ ecdfIEEE<- function(n,f_from,f_to){
 
 
 ecdfIEEE(10,1,3)
+if(TRUE){
 ecdfIEEE(10,4,10)
 ecdfIEEE(10,11,20)
 ecdfIEEE(10,21,30)
@@ -149,3 +172,4 @@ ecdfIEEE(100,4,10)
 ecdfIEEE(100,11,20)
 ecdfIEEE(100,21,30)
 ecdfIEEE(100,1,30)
+}
