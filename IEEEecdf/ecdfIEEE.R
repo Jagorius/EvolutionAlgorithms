0f44953aaa7a0bc7bf94641a7e48167edf599821
Jagorius <Jagorius@gmail.com>
2017-12-31 12:59:02 +0100

    ecdf combined plots

--- IEEEecdf/ecdfIEEE.R
+++ IEEEecdf/ecdfIEEE.R
@@ -23,9 +23,10 @@ ecdfIEEE<- function(n,f_from,f_to){
 
   ecdfValues <- list()
   budgetSteps <- c(0.01, 0.02, 0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)*log10(10000)
-  colors <- rainbow(16) 
-  colors[3] <- colors[16]
-  linetype <- c(c(1:10),c(6:1))
+  #colors <- rainbow(16) 
+  #colors[3] <- colors[16]
+  colors <- c(rep("black",3),rep("grey",10))
+  linetype <- c(c(1:6),c(1:6))
   plotchar <- seq(15,15+10,1)
 
   ecdfMaxSucess <- 0
@@ -126,13 +127,12 @@ ecdfIEEE<- function(n,f_from,f_to){
     ecdfMaxSucess <- ecdfMaxSucess + length(ecdfValues[[p]])*51
   }
   
-  setEPS()
-  postscript( paste("Problems",F_from,"-",F_to,",N=",N,".eps",sep=""), width = 5, height = 5)
+  isXaxt <- if(N==100) "s" else "n"
+  isYaxt <- if(F_from==1) "s" else "n"
+  #setEPS()
+  #postscript( paste("Problems",F_from,"-",F_to,",N=",N,".eps",sep=""), width = 5, height = 5)
                                               #xlab="log10 of (f-evals / dimension)",ylab="Proportion of function + target pairs",
-  plot(budgetSteps,minCountDESv2/(ecdfMaxSucess),xlab="",ylab="",ylim=c(0, 1),type="b", lwd=2,lty=linetype[1], col=colors[1], pch=plotchar[1])
-  lines(budgetSteps,minCountCmaes_pure/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[2], col=colors[2], pch=plotchar[2])
-  lines(budgetSteps,minCountRB_IPOP_CMA_ES/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[3], col=colors[3], pch=plotchar[3])
-  lines(budgetSteps,minCountjso/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[4], col=colors[4], pch=plotchar[4])
+  plot(budgetSteps,minCountjso/(ecdfMaxSucess),xlab="",ylab="",ylim=c(0, 1),type="b", lwd=2,lty=linetype[4], col=colors[4], pch=plotchar[4], xaxt=isXaxt,  yaxt=isYaxt)
   lines(budgetSteps,minCountshadeSpacma/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[5], col=colors[5], pch=plotchar[5])
   lines(budgetSteps,minCountEbo/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[6], col=colors[6], pch=plotchar[6])
   lines(budgetSteps,minCountIde/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[7], col=colors[7], pch=plotchar[7])
@@ -141,15 +141,58 @@ ecdfIEEE<- function(n,f_from,f_to){
   lines(budgetSteps,minCountDyypo/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[10], col=colors[10], pch=plotchar[10])
   lines(budgetSteps,minCountTlbofl/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[11], col=colors[11], pch=plotchar[11])
   
-  legend(-0.08, 1.03, c('DESv2', 'CMAES','RB_IPOP_CMA_ES','jSO','LSHADE_SPACMA','EBOwithCMAR','IDEbestNsize','PPSO','MM_OED','DYYPO','TLBO-FL'), text.font=2, cex=0.8, col=colors[1:11],pch=plotchar[1:11], lty=linetype[1:11] )
+  lines(budgetSteps,minCountRB_IPOP_CMA_ES/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[3], col=colors[3], pch=plotchar[3])
+  lines(budgetSteps,minCountCmaes_pure/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[2], col=colors[2], pch=plotchar[2])
+  lines(budgetSteps,minCountDESv2/(ecdfMaxSucess),type="b", lwd=2,lty=linetype[1], col=colors[1], pch=plotchar[1])
   
-  dev.off()
+  #legend(-0.08, 1.03, c('DESv2', 'CMAES','RB_IPOP_CMA_ES','jSO','LSHADE_SPACMA','EBOwithCMAR','IDEbestNsize','PPSO','MM_OED','DYYPO','TLBO-FL'), text.font=2, cex=0.8, col=colors[1:11],pch=plotchar[1:11], lty=linetype[1:11] )
   
+  #dev.off()
+  #return(rplot)
 }
 
+combinedPlot <- function(){
+  #colors <- rainbow(16) 
+  #colors[3] <- colors[16]
+  colors <- c(rep("black",3),rep("grey",10))
+  linetype <- c(c(1:6),c(1:6))
+  plotchar <- seq(15,15+10,1)
+  
+  setEPS()
+  postscript( "combinedPlot.eps", width = 20, height = 18)
+  layout(mat = matrix(1:16,nrow = 4,ncol = 4,byrow = TRUE),heights = c(1,1,1,1,0.2))
+  par(oma=c(10, 6, 0, 6), mar=4*c(.1,.1,.1,.1), cex=1, las=1)
+  ecdfIEEE(10,1,3)
+  ecdfIEEE(10,4,10)
+  ecdfIEEE(10,11,20)
+  ecdfIEEE(10,21,30)
 
+  ecdfIEEE(30,1,3)
+  ecdfIEEE(30,4,10)
+  ecdfIEEE(30,11,20)
+  ecdfIEEE(30,21,30)
+
+  ecdfIEEE(50,1,3)
+  ecdfIEEE(50,4,10)
+  ecdfIEEE(50,11,20)
+  ecdfIEEE(50,21,30)
+
+  ecdfIEEE(100,1,3)
+  ecdfIEEE(100,4,10)
+  ecdfIEEE(100,11,20)
+  ecdfIEEE(100,21,30)
+  mtext("log10 of (f-evals / dimension)", 1, 0, outer=TRUE,padj=3,cex=2)
+  mtext("Proportion of function + target pairs", 2, 3, outer=TRUE, las=0,cex=2)
+  
+  par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
+  plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
+  legend("bottom", c('DESv2', 'CMAES','RB_IPOP_CMA_ES','jSO','LSHADE_SPACMA','EBOwithCMAR','IDEbestNsize','PPSO','MM_OED','DYYPO','TLBO-FL'), text.font=2, cex=1.5, col=colors[1:11],pch=plotchar[1:11], lty=linetype[1:11], ncol=6)
+  
+  dev.off()
+}
+
+if(FALSE){
 ecdfIEEE(10,1,3)
-if(TRUE){
 ecdfIEEE(10,4,10)
 ecdfIEEE(10,11,20)
 ecdfIEEE(10,21,30)
